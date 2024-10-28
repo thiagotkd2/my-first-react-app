6ae88d500cf365374652d90a44fb42fc4dbbae9e
thiagotkd2 <thiagop.pf.moraes@gmail.com>
2024-10-28 18:28:36 -0300

    Adapted JS tic-tac-toe to next.js react app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,127 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import { useState } from "react";
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+interface SquareProps {
+	value:string | null;
+	onSquareClick:()=> void;
+}
+// when to use interface and type
+interface BoardProps {
+	xIsNext:boolean;
+	squares:(null | string)[];
+	onPlay: (squares:(null | string)[]) => void;
+}
+
+
+function Square({value, onSquareClick}:SquareProps){ // : {value:string; onSquareClick: ()=> void;}) 
+	return <button className="square" onClick={onSquareClick}>{value}</button>
+}
+
+function Board({xIsNext,squares,onPlay}:BoardProps){
+	function handleClick(i:number):void{
+		if(squares[i] || calculateWinner(squares))
+			return
+		const nextSquares:BoardProps["squares"] = squares.slice()
+		if(xIsNext){
+			nextSquares[i]='X'
+		}else{
+			nextSquares[i]='O'
+		}
+
+		onPlay(nextSquares)
+
+	}
+
+	const winner = calculateWinner(squares)
+	let status:string
+	if(winner){
+		status = "Winner: " + winner
+	}else{
+		status = "Next player: " + (xIsNext? "X": "O")
+	}
+
+	return (
+		<>
+			<div className="status">{status}</div>
+			<div className="board-row">
+				<Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+				<Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+				<Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+			</div>
+			<div className="board-row">
+				<Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+				<Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+				<Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+			</div>
+			<div className="board-row">
+				<Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+				<Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+				<Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+			</div>
+		</>
+	)
+}
+
+export default function Game(){
+	const [history, setHistory] = useState<BoardProps["squares"][]>([Array(9).fill(null)]);
+	const [currentMove, setCurrentMove] = useState<number>(0)
+	const xIsNext:boolean = currentMove%2===0
+	const currentSquares = history[currentMove]
+
+	function handlePlay(nextSquares:BoardProps["squares"]):void{
+		const nextHistory:BoardProps["squares"][] = ([...history.slice(0, currentMove+1), nextSquares])
+		setHistory(nextHistory)
+		setCurrentMove(nextHistory.length-1)
+	}
+
+	function jumpTo(nextMove:number):void{
+		setCurrentMove(nextMove)
+	}
+
+	const moves= history.map((squares:BoardProps["squares"], move:number) =>{
+		let description
+		if(move>0){
+			description = "Go to move # " + move
+		}else{
+			description = "Go to game start"
+		}
+		return(
+			<li key={move}>
+				<button onClick={() => jumpTo(move)}>{description}</button>
+			</li>
+		);
+	});
+
+	return (
+		<div className="game">
+			<div className="game-board">
+				<Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+			</div>
+			<div className="game-info">
+				<ol>{moves}</ol>
+			</div>
+		</div>
+	)
+}
+
+
+function calculateWinner(squares:BoardProps["squares"]): null | string {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6]
+	];
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c]:number[] = lines[i];
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
 }
